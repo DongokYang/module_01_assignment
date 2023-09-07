48440f18f62397e2d37fd4367560fb89f2e57646
DongokYang <dyang2@academic.rrc.ca>
2023-09-07 15:03:34 -0500

    Initial commit: Set up project directory and file structure

---
+++ README.md
@@ -0,0 +1,9 @@
+# hello
+
+## Description
+    first assignment at COMP 1327
+## Author
+    Dongok Yang
+
+## Assignment
+    module_01_assignment
---
+++ hello.py
@@ -0,0 +1,8 @@
+"""
+Description : First assignment in COMP1327
+Author : Dongok Yang
+Date : 2023.09.07
+Usage : To upload github
+"""
+
+print("hello")
